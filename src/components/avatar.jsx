f66735bba3aa6ed46f52dae23f8c4a31f3f3bd90
bosees <rladuddlf3@naver.com>
2022-08-09 21:33:06 +0900

    merge

--- src/components/avatar.jsx
+++ src/components/avatar.jsx
@@ -1,7 +1,11 @@
-export const Blog = () => {
+export const Avatar = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-            blogasdasdasd
-        </h1>
+        <div className="flex flex-col bg-red-500 justify-center items-center">
+            <img className="w-36 rounded-full" src="/img/Git.svg"/>
+            <a className="text-2xl">BOSEES</a>
+            <div className="bg-make-dark-green w-44 rounded-lg p-2 m-2">
+                <i className="text-1xl">Blockchain Developer</i>
+            </div>
+        </div>
     );
 }
--- src/view/selfIntro.jsx
+++ src/view/selfIntro.jsx
@@ -1,7 +1,15 @@
+import { Avatar } from "../components/avatar";
+
 export const SelfIntro = () => {
     return (
-        <h1 className="text-3xl font-bold underline text-make-light-green">
-            self intro
-        </h1>
+        <div>
+            <Avatar />
+            <h1 className="text-3xl font-bold text-make-light-green">
+                안녕하세요.
+            </h1>
+            <h2 className="text-2xl font-bold text-make-light-green">
+                블록체인 프론트엔드 개발자 김영일입니다.
+            </h2>
+        </div>
     );
 }
