a12a11928cc5b5ecac1a4fc038e987661cf0fc10
kaen <kusdsuna@naver.com>
2022-08-09 21:17:44 +0900

    style: img file upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!asdasd
-    </h1>
-  );
+	const [toggle, setToggle] = useState(true);
+
+	return (
+		<div className="flex flex-col justify-center align-center w-screen h-screen">
+			{toggle ? (
+				<div className="flex flex-col justify-center align-center w-full h-full bg-neutral-900 text-point">
+					<h1 className="flex shadow-point-600">Hello World</h1>
+					<img className="flex" src="/public/img/profile.png" alt="_" />
+					<span className="flex">Hello World Hello World</span>
+					<button onClick={() => setToggle(false)}>PRESS START</button>
+				</div>
+			) : (
+				<div></div>
+			)}
+		</div>
+	);
 }
 
 export default App;
