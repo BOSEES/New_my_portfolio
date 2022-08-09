3fadc8a66e691b739814a7935d052058eb104aa8
kaen <kusdsuna@naver.com>
2022-08-09 21:32:04 +0900

    style: png file upload

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ const App = () => {
 			<SelfIntro />
 			<div className="flex flex-col justify-center align-center w-screen h-screen">
 				{toggle ? (
-					<div className="flex flex-col justify-center align-center w-full h-full bg-neutral-900 text-point">
-						<h1 className="flex shadow-point-600">Hello World</h1>
+					<div className="flex flex-col justify-center align-center w-full h-full bg-neutral-900 text-make-light-green">
+						<h1 className="flex shadow-make-light-green-600">Hello World</h1>
 						<img className="flex" src="/public/img/profile.png" alt="_" />
 						<span className="flex">Hello World Hello World</span>
 						<button onClick={() => setToggle(false)}>PRESS START</button>
