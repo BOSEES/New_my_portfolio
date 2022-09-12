753d2b36f3157a4d488b544032be74950564e3c4
bosees <rladuddlf3@naver.com>
2022-09-12 18:56:48 +0900

    수정

--- src/components/avatar.jsx
+++ src/components/avatar.jsx
@@ -1,7 +1,7 @@
 export const Avatar = () => {
     return (
         <div className="w-48 flex flex-col  justify-center items-center mr-8">
-            <img className="w-28 rounded-full" src="/img/Git.png" alt="avatar"/>
+            <img className="w-28 rounded-full" src="img/profile.jpeg" alt="avatar"/>
             <h2 className="text-2xl text-white">BOSEES</h2>
             <div className="bg-white w-44 rounded-lg p-2 m-2">
                 <i className="text-1xl">Blockchain Developer</i>
--- src/view/splash.jsx
+++ src/view/splash.jsx
@@ -6,10 +6,10 @@ export const Splash = ({ setToggle }) => {
             </h1>
             <img
                 className="flex w-96 my-0 mx-auto bottom-0 z-0 absolute bottom-0 left-1/2 transform -translate-x-1/2"
-                src="/img/profile.png"
+                src="img/profile.png"
                 alt="_"
             />
-            <span className="flex justify-center text-make-dark-green text-5xl mb-56 z-10">
+            <span className="flex justify-center text-make-light-green text-5xl mb-56 z-10">
                 Hello World Hello World
             </span>
             <button
