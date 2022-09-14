f4095e5bd58ee48c89f39fde7219cca50314063e
bosees <rladuddlf3@naver.com>
2022-09-14 10:53:10 +0900

    fix: Update file name

--- src/components/avatar.jsx
+++ src/components/avatar.jsx
@@ -1,12 +1,12 @@
 export const Avatar = () => {
     return (
         <div className="w-48 flex flex-col  justify-center items-center mr-8">
-            <img className="w-28 rounded-full" src="img/profile.jpeg" alt="avatar"/>
+            <img className="w-28 h-28 rounded-full" src="img/profile.jpeg" alt="avatar"/>
                 <a href="https://github.com/BOSEES" target="_blank" rel="noreferrer">
                     <div className="flex items-center">
                         <h2 className="text-2xl text-white">BOSEES</h2>
                         <div className="w-6 h-6 ml-1">
-                                <img className="w-full rounded-full" src="img/github.png" alt="github"/>
+                                <img className="rounded-full w-5 h-5" src="img/github.png" alt="github"/>
                         </div>
                     </div>
                 </a>
